12ff6d2ea285fcca37329ecc5523759fd30bb456
Kanhu <kanhu.kcn@gmail.com>
2021-06-28 22:52:40 +0530

    learn the typecast of python examples

---
+++ python lecture/input_output.py
@@ -0,0 +1,8 @@
+print("hay!!,Enter some Age")
+Age = input ()
+print("hay !!, You have enter", "you are" , Age,"year old")
+print("Hay !! Please Enter your name")
+name = input ()
+print("Hi ", name ,"how are you today")
+
+
---
+++ python lecture/typecast.py
@@ -0,0 +1,23 @@
+"""
+This code will shou you haow do typecasting
+Convert one variable to another variable
+Just wrap the variable into int(), str() or float()
+
+
+"""
+a = "5"
+print("before",type(a))
+
+a = int(a)
+
+print("after",type(a))
+
+print("hay! enter a 1st number")
+
+no1 = int(input())
+
+print("hay ! Enter 2nd number")
+
+no2 = int(input())
+
+print("your result is ",no1 + no2 )
