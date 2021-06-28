255f4d5cde55fccb90e4b53d114fc8e2925a1919
Kanhu <kanhu.kcn@gmail.com>
2021-06-28 07:27:13 +0530

    First coading of python

---
+++ python lecture/intro.py
@@ -0,0 +1,26 @@
+print("hello world")
+"""
+Today I'm going to learn python data type
+There are 3 types of data
+int
+str
+float
+To define the class we are going to use command type()
+>>> type(numbers)
+>> <clsss,int>
+>>> type(alphabets)
+>> <class,str>
+>>> type(decemles)
+>> <class,float>
+
+
+
+"""
+integer_variable = 5
+print("The type of a is:",type(integer_variable))
+
+string_variable = "kanhu"
+print("the type of b is:",type(string_variable))
+
+float_variable = 1.5
+print("the type of c is:",type(float_variable))
